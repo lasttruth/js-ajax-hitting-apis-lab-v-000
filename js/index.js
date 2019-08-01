8996b9efde45f2ad83b3a76d3c831802c5868c99
Robin Doirin <robindoirin@gmail.com>
2019-08-01 17:32:31 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,46 +1,53 @@
 // your code here
 function getRepositories() {
     const req = new XMLHttpRequest();
-    req.addEventListener('load', showRepositories);
-    req.open('GET', 'https://api.github.com/users/octocat/repos');
+    const username = document.querySelector('#username').value
+    req.addEventListener('load', displayRepositories);
+    req.open('GET', `https://api.github.com/users/${username}/repos`);
     req.send();
-  }
+}
 
-  function showRepositories() {
+function displayRepositories() {
     var repos = JSON.parse(this.responseText);
     console.log(repos);
-    const repoList = `<ul>${repos
-      .map(
-        r =>
-          '<li>' +
-          r.name +
-          ' - <a href="#" data-repo="' +
-          r.name +
-          '" onclick="getCommits(this)">Get Commits</a></li>'
-      )
-      .join('')}</ul>`;
+    const repoList = `<ol>${repos.map(r => '<li>' + r.name + ` - <a href="${r.html_url}">Go to Repo</a>` + ` - <a href="#" data-repository=${r.name} data-username=${r.owner.login} onclick="getCommits(this)">Commits</a>` + ` - <a href="#" data-repo=${r.name} data-username=${r.owner.login} onclick="getBranches(this)">Branches</a></li>`).join('')}</ol>`;
     document.getElementById('repositories').innerHTML = repoList;
-  }
+}
 
-  function getCommits(el) {
-    const name = el.dataset.repo;
+function getCommits(el) {
+    const repository = el.dataset.repository;
+    const username = el.dataset.username;
     const req = new XMLHttpRequest();
-    req.addEventListener('load', showCommits);
-    req.open('GET', 'https://api.github.com/repos/octocat/' + name + '/commits');
+    req.addEventListener('load', displayCommits);
+    req.open('GET', 'https://api.github.com/repos/' + username + '/' + repository + '/commits');
     req.send();
-  }
+}
 
-  function showCommits() {
+function displayCommits() {
     const commits = JSON.parse(this.responseText);
-    const commitsList = `<ul>${commits
-      .map(
-        commit =>
-          '<li><strong>' +
-          commit.author.login +
-          '</strong> - ' +
-          commit.commit.message +
-          '</li>'
-      )
-      .join('')}</ul>`;
-    document.getElementById('commits').innerHTML = commitsList;
-  } 
+    const commitsList = `<ul>${commits.map(commit => '<li>' + commit.commit.author.name + ' - ' + decideLogin(commit) + ' - ' + commit.commit.message + '</li>').join('')}</ul>`;
+    document.getElementById('details').innerHTML = commitsList;
+}
+
+function decideLogin(commit) {
+    if (commit.author) {
+        return commit.author.login;
+    } else {
+        return "Login Missing";
+    }
+}
+
+function getBranches(el) {
+    const repository = el.dataset.repository;
+    const username = el.dataset.username;
+    const req = new XMLHttpRequest();
+    req.addEventListener ('load', displayBranches);
+    req.open('GET', 'https://api.github.com/repos/' + username + '/' + repository + '/branches');
+    req.send();
+}
+
+function displayBranches() {
+    const branches = JSON.parse(this.responseText);
+    const branchList = `<ul>${branches.map(branch => '<li>' + branch.name + '</li>').join('')}</ul>`
+    document.getElementById('details').innerHTML = branchList;
+} 
